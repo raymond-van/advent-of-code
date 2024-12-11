aa415377818b90b616ada41d4ba0896d9734172f
raymond-van <raymondxvan@gmail.com>
2024-12-11 16:11:38 -0500

    day 5

---
+++ 2024/5a.cpp
@@ -0,0 +1,86 @@
+#include <csignal>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int ans = 0;
+int m, n;
+vector<vector<int>> rules;
+vector<vector<int>> updates;
+void read_input(const string &file_name) {
+  ifstream file(file_name);
+  if (!file.is_open()) {
+    cerr << "Failed to open the file: " << file_name << '\n';
+    exit(1);
+  }
+  string line;
+  char delimiter = '|';
+  while (getline(file, line)) {
+    istringstream iss(line);
+    string num;
+    vector<int> row;
+    if (line.size() == 0) {
+      delimiter = ',';
+      continue;
+    }
+    while (getline(iss, num, delimiter)) {
+      row.push_back(stoi(num));
+    }
+    if (delimiter == '|')
+      rules.push_back(row);
+    else
+      updates.push_back(row);
+  }
+}
+
+void solve() {
+  try {
+    unordered_map<int, unordered_set<int>> mp;
+    for (int i = 0; i < m; i++) {
+      mp[rules[i][0]].insert(rules[i][1]);
+    }
+    for (int i = 0; i < n; i++) {
+      int o = updates[i].size();
+      bool valid = true;
+      for (int j = 0; j < o and valid; j++) {
+        int num = updates[i][j];
+        for (int k = 0; k < j and valid; k++) {
+          int prev = updates[i][k];
+          if (mp[num].count(prev)) {
+            valid = false;
+          }
+        }
+      }
+      if (valid) {
+        int mid = o / 2;
+        ans += updates[i][mid];
+        for (int z = 0; z < o; z++) cout << updates[i][z] << ' ';
+      }
+    }
+  } catch (const exception &e) {
+    cerr << "Caught exception: " << e.what() << '\n';
+    throw e;
+  }
+}
+
+int main() {
+  string path = filesystem::current_path() / "2024/inputs/5.txt";
+  read_input(path);
+  try {
+    m = rules.size();
+    n = updates.size();
+    solve();
+    cout << "ans: " << ans << '\n';
+  } catch (const exception &e) {
+    cerr << "Caught exception: " << e.what() << '\n';
+    return 1;
+  }
+  return 0;
+}
--- 2024/template.cpp
+++ 2024/template.cpp
@@ -7,37 +7,52 @@
 
 using namespace std;
 
-int ans = -1;
+int ans = 0;
 int m, n;
 vector<vector<int>> input;
 void read_input(const string &file_name) {
-  ifstream file(file_name);
-  if (!file.is_open()) {
-    cerr << "Failed to open the file: " << file_name << '\n';
-    exit(1);
-  }
-  string line;
-  vector<vector<int>> rows;
-  while (getline(file, line)) {
-    istringstream iss(line);
-    int number;
-    vector<int> row;
-    while (iss >> number) {
-      row.push_back(number);
+  try {
+
+    ifstream file(file_name);
+    if (!file.is_open()) {
+      cerr << "Failed to open the file: " << file_name << '\n';
+      exit(1);
     }
-    rows.push_back(row);
+    string line;
+    vector<vector<int>> rows;
+    while (getline(file, line)) {
+      istringstream iss(line);
+      int number;
+      vector<int> row;
+      while (iss >> number) {
+        row.push_back(number);
+      }
+      rows.push_back(row);
+    }
+  } catch (const exception &e) {
+    cerr << "Caught exception reading input: " << e.what() << '\n';
+    throw e;
   }
 }
 
 void solve() {
-  
+  try {
+
+  } catch (const exception &e) {
+    cerr << "Caught exception solving: " << e.what() << '\n';
+    throw e;
+  }
 }
 
 int main() {
-  string path = filesystem::current_path()/"2024/inputs/_.txt";
-  read_input(path);
-  solve();
-  cout << "ans: " << ans << endl;
-
+  try {
+    string path = filesystem::current_path() / "2024/inputs/_.txt";
+    read_input(path);
+    solve();
+    cout << "ans: " << ans << endl;
+  } catch (const exception &e) {
+    cerr << "Caught exception (main): " << e.what() << '\n';
+    return 1;
+  }
   return 0;
 }
